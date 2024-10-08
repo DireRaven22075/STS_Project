1d9204a9c2ad74b34a9f799a855a0adb6e0d7f7c
Han Yoon Soo <hanhs9803753@gmail.com>
2024-10-08 16:37:55 +0900

    Feature : Add Field SYSTEM

---
+++ Assets/Scripts/Data/Entity.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+namespace STS.SerializedData
+{
+    [CreateAssetMenu(fileName = "Entity", menuName = "Entity", order = 0)]
+    public class Entity : ScriptableObject
+    {
+        private Sprite image;
+
+
+    }
+}
